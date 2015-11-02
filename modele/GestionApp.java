310548b42b89ab3594408751346f74a6878b127a
fcallet <francois.callet@insa-lyon.fr>
2015-11-02 10:19:20 +0100

    Correcting Intersection class

--- modele/GestionApp.java
+++ modele/GestionApp.java
@@ -38,7 +38,94 @@ public class GestionApp {
         GestionApp ga = new GestionApp();
         
         //Intersections
-    	Intersection i1 = new Intersection(0,63,100);
+
+        Intersection i0 = new Intersection(0,63,100);
+        Intersection i1 = new Intersection(1,88,171);
+        Intersection i2 = new Intersection(2,103,248);
+        Intersection i3 = new Intersection(3,65,310);
+        Intersection i4 = new Intersection(4,77,350);
+        Intersection i5 = new Intersection(5,83,403);
+        Intersection i6 = new Intersection(6,87,843);
+        Intersection i7 = new Intersection(7,45,579);
+        Intersection i8 = new Intersection(8,43,619);
+        Intersection i9 = new Intersection(9,59,699);
+        Intersection i10 = new Intersection(10,154,95);
+
+        //Troncons
+        Troncon t1 = new Troncon(10,i0,i1);
+        Troncon t2 = new Troncon(10,i0,i2);
+        i0.ajouterTroncon(t1);
+        i0.ajouterTroncon(t2);
+
+        Troncon t3 = new Troncon(10,i1,i0);
+        Troncon t4 = new Troncon(10,i1,i3);
+        i1.ajouterTroncon(t3);
+        i1.ajouterTroncon(t4);
+
+        Troncon t5 = new Troncon(10,i2,i0);
+        Troncon t6 = new Troncon(10,i2,i3);
+        Troncon t7 = new Troncon(10,i2,i4);
+        i2.ajouterTroncon(t5);
+        i2.ajouterTroncon(t6);
+        i2.ajouterTroncon(t7);
+
+        Troncon t8 = new Troncon(10,i3,i2);
+        Troncon t9 = new Troncon(10,i3,i1);
+        Troncon t10 = new Troncon(10,i3,i7);
+        i3.ajouterTroncon(t8);
+        i3.ajouterTroncon(t9);
+        i3.ajouterTroncon(t10);
+
+        Troncon t11 = new Troncon(10,i4,i5);
+        Troncon t12 = new Troncon(10,i4,i2);
+        i4.ajouterTroncon(t11);
+        i4.ajouterTroncon(t12);
+
+        Troncon t13 = new Troncon(10,i5,i4);
+        Troncon t14 = new Troncon(10,i5,i6);
+        i5.ajouterTroncon(t13);
+        i5.ajouterTroncon(t14);
+
+        Troncon t15 = new Troncon(10,i6,i5);
+        Troncon t16 = new Troncon(10,i6,i7);
+        i6.ajouterTroncon(t15);
+        i6.ajouterTroncon(t16);
+
+        Troncon t17 = new Troncon(10,i7,i6);
+        Troncon t18 = new Troncon(10,i7,i8);
+        Troncon t19 = new Troncon(10,i7,i3);
+        i7.ajouterTroncon(t17);
+        i7.ajouterTroncon(t18);
+        i7.ajouterTroncon(t19);
+
+        Troncon t20 = new Troncon(10,i8,i9);
+        Troncon t21 = new Troncon(10,i8,i10);
+        Troncon t22 = new Troncon(10,i8,i7);
+        i8.ajouterTroncon(t20);
+        i8.ajouterTroncon(t21);
+        i8.ajouterTroncon(t22);
+
+        Troncon t23 = new Troncon(10,i9,i8);
+        Troncon t24 = new Troncon(10,i9,i10);
+        i9.ajouterTroncon(t23);
+        i9.ajouterTroncon(t24);
+
+        Troncon t25 = new Troncon(10,i10,i8);
+        Troncon t26 = new Troncon(10,i10,i9);
+        i10.ajouterTroncon(t25);
+        i10.ajouterTroncon(t26);
+
+        ga.getPlan().addIntersection(i0);
+        ga.getPlan().addIntersection(i1);
+        ga.getPlan().addIntersection(i2);
+        ga.getPlan().addIntersection(i3);
+        ga.getPlan().addIntersection(i4);
+        ga.getPlan().addIntersection(i5);
+        ga.getPlan().addIntersection(i6);
+        ga.getPlan().addIntersection(i7);
+        ga.getPlan().addIntersection(i8);
+        ga.getPlan().addIntersection(i9);
+        ga.getPlan().addIntersection(i10);
     	
     	//livraisons
     	Livraison l1 = new Livraison(ga.getPlan().getIntersectionById(3));
@@ -72,6 +159,7 @@ public class GestionApp {
     	fl3.addLivraison(l7); fl3.addLivraison(l8); fl3.addLivraison(l9);
     	
     	ga.getPlan().addIntersection(i1);
+    	System.out.println(ga.getPlan().getHauteur());
     	
     }
     
