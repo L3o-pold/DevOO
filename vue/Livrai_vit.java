dcf6d124e975d381aa0d41c54c2f3a58b9aef1ae
rboury <rboury.rb@gmail.com>
2015-11-04 11:44:32 +0100

    Adding action listeners, renaming Swing items, and adding Observateur class

--- vue/Livrai_vit.java
+++ vue/Livrai_vit.java
@@ -1,11 +1,41 @@
+package vue;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import java.awt.Color;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import java.awt.Button;
+import java.awt.Choice;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Canvas;
+import java.awt.SystemColor;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.AbstractListModel;
 
 public class Livrai_vit {
 
-	private JFrame frame;
-
+	private JFrame mainFrame;
+	
+	private JLabel lblInstruction;
+	
+	
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -14,7 +44,7 @@ public class Livrai_vit {
 			public void run() {
 				try {
 					Livrai_vit window = new Livrai_vit();
-					window.frame.setVisible(true);
+					window.mainFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,9 +63,197 @@ public class Livrai_vit {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame = new JFrame();
+		mainFrame.setTitle("Livrai'vit");
+		mainFrame.setResizable(false);
+		mainFrame.setBounds(100, 100, 1185, 617);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panelLivraison = new JPanel();
+		panelLivraison.setBackground(Color.YELLOW);
+		mainFrame.getContentPane().add(panelLivraison, BorderLayout.EAST);
+		panelLivraison.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelEdition = new JPanel();
+		panelLivraison.add(panelEdition, BorderLayout.NORTH);
+		
+		JButton btnSupression = new JButton("");
+		btnSupression.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				System.out.println("Supression Cliqué");
+			}
+		});
+		btnSupression.setIcon(new ImageIcon(Livrai_vit.class.getResource("/javax/swing/plaf/metal/icons/ocean/paletteClose-pressed.gif")));
+		panelEdition.add(btnSupression);
+		
+		JButton btnNouveau = new JButton("+");
+		btnNouveau.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnNouveau.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panelEdition.add(btnNouveau);
+		
+		JButton btnMonter = new JButton("");
+		btnMonter.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnMonter.setIcon(new ImageIcon(Livrai_vit.class.getResource("/com/sun/javafx/scene/control/skin/caspian/dialog-fewer-details.png")));
+		panelEdition.add(btnMonter);
+		
+		JButton btnDescendre = new JButton("");
+		btnDescendre.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnDescendre.setIcon(new ImageIcon(Livrai_vit.class.getResource("/com/sun/javafx/scene/control/skin/caspian/dialog-more-details.png")));
+		panelEdition.add(btnDescendre);
+		
+		JList listLivraison = new JList();
+		listLivraison.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Pierre", "Paul", "Jacques"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		listLivraison.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JList listLivraison = (JList)e.getSource();
+		        int index = listLivraison.locationToIndex(e.getPoint());
+		        System.out.println(index);        
+			}
+		});
+		listLivraison.setBackground(new Color(173, 216, 230));
+		panelLivraison.add(listLivraison, BorderLayout.CENTER);
+		
+		JPanel panel_1 = new JPanel();
+		mainFrame.getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelInstruction = new JPanel();
+		panelInstruction.setBackground(Color.GRAY);
+		panel_1.add(panelInstruction, BorderLayout.SOUTH);
+		panelInstruction.setLayout(new BorderLayout(0, 0));
+		
+		this.lblInstruction = new JLabel("New label");
+		this.lblInstruction.setForeground(Color.WHITE);
+		panelInstruction.add(this.lblInstruction, BorderLayout.CENTER);
+		
+		JPanel panelValidation = new JPanel();
+		panelValidation.setBackground(Color.GRAY);
+		panelValidation.setForeground(Color.BLACK);
+		panelInstruction.add(panelValidation, BorderLayout.EAST);
+		panelValidation.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton btnValider = new JButton("Valider");
+		btnValider.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnValider.setEnabled(false);
+		panelValidation.add(btnValider);
+		
+		JButton btnAnnuler = new JButton("Annuler");
+		btnAnnuler.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnAnnuler.setEnabled(false);
+		panelValidation.add(btnAnnuler);
+		
+		JPanel panelToolbar = new JPanel();
+		FlowLayout fl_panelToolbar = (FlowLayout) panelToolbar.getLayout();
+		fl_panelToolbar.setAlignment(FlowLayout.LEFT);
+		panel_1.add(panelToolbar, BorderLayout.NORTH);
+		
+		JMenuBar menuBar = new JMenuBar();
+		panelToolbar.add(menuBar);
+		
+		JMenu mnMenu = new JMenu("Menu");
+		menuBar.add(mnMenu);
+		
+		JMenuItem mntmChargerPlan = new JMenuItem("Charger plan...");
+		mntmChargerPlan.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Charged plan");
+				lblInstruction.setText("plan chargé");
+			}
+		});
+		
+		mnMenu.add(mntmChargerPlan);
+		
+		JMenuItem mntmChargerLivraison = new JMenuItem("Charger livraison...");
+		mntmChargerLivraison.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Charged livraison");
+			}
+		});
+		mnMenu.add(mntmChargerLivraison);
+		
+		JMenuItem mntmReset = new JMenuItem("Reset");
+		mntmReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Reset");
+			}
+		});
+		mnMenu.add(mntmReset);
+		
+		JMenuItem mntmExporter = new JMenuItem("Exporter feuille de route");
+		mntmExporter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Export");
+			}
+		});
+		mnMenu.add(mntmExporter);
+		
+		JButton btnUndo = new JButton("");
+		btnUndo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnUndo.setIcon(new ImageIcon(Livrai_vit.class.getResource("/com/sun/javafx/scene/web/skin/Undo_16x16_JFX.png")));
+		btnUndo.setToolTipText("Annuler (Ctrl-Z)");
+		btnUndo.setHorizontalAlignment(SwingConstants.RIGHT);
+		menuBar.add(btnUndo);
+		
+		JButton btnRedo = new JButton("");
+		btnRedo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnRedo.setIcon(new ImageIcon(Livrai_vit.class.getResource("/com/sun/javafx/scene/web/skin/Redo_16x16_JFX.png")));
+		btnRedo.setToolTipText("R\u00E9p\u00E9ter (Ctrl-Y)");
+		btnRedo.setHorizontalAlignment(SwingConstants.RIGHT);
+		menuBar.add(btnRedo);
+		
+		JPanel panelPlan = new JPanel();
+		panel_1.add(panelPlan, BorderLayout.CENTER);
+		panelPlan.setLayout(null);
+		
+		Canvas canvas = new Canvas();
+		canvas.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		canvas.setBackground(new Color(255, 255, 255));
+		canvas.setBounds(10, 10, 950, 500);
+		panelPlan.add(canvas);
 	}
-
 }
--- vue/Test.java
+++ vue/Test.java
@@ -1,3 +1,4 @@
+package vue;
 
 public class Test {
 
