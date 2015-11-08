c32e442504b669cadccadb1c73efb677cf7df5e5
Seiji SCHOCH <seijischoch@hotmail.fr>
2015-11-08 04:57:44 +0300

    Added dessinerTronconsTournee with  progressive color and arrows

--- vue/VueGraphique.java
+++ vue/VueGraphique.java
@@ -7,11 +7,14 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.MediaTracker;
+import java.awt.Polygon;
 import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -63,13 +66,7 @@ public class VueGraphique extends Canvas implements Observer {
 		if (this.plan.getTournee() != null)
 		{
 			dessinerEntrepot(g2, this.plan.getTournee().getEntrepot());
-			for(Etape e : this.plan.getTournee().getEtapes())
-			{
-				for( Troncon t : e.getTroncons())
-				{
-					dessinerTronconTournee(g2,t);
-				}
-			}
+			dessinerTronconsTournee(g2);
 		}
 		
 	}
@@ -89,17 +86,40 @@ public class VueGraphique extends Canvas implements Observer {
 		g.draw(new Line2D.Double(x1, y1, x2, y2));
 	}
 	
-	public void dessinerTronconTournee(Graphics2D g, Troncon t) {
-		double x1 = t.getDepart().getX()*this.widthRatio;
-		double y1 = t.getDepart().getY()*this.heightRatio;
-		double x2 = t.getArrivee().getX()*this.widthRatio;
-		double y2 = t.getArrivee().getY()*this.heightRatio;
-		g.setColor(Color.RED);
-		g.setStroke(new BasicStroke((2f),
-						BasicStroke.CAP_BUTT,
-						BasicStroke.JOIN_ROUND));
-		System.out.println("aa");
-		g.draw(new Line2D.Double(x1, y1, x2, y2));
+	public void dessinerTronconsTournee(Graphics2D g) {
+		
+		int nbTroncons=0;
+		for(Etape e : this.plan.getTournee().getEtapes() )
+		{
+			nbTroncons += e.getTroncons().size();
+		}
+		
+		float red = 255;
+		float green = 255;
+		float blue = 0;
+		
+		float step = 255 / nbTroncons;
+		
+		for(Etape e : this.plan.getTournee().getEtapes() )
+		{
+			for(Troncon t : e.getTroncons() )
+			{
+				double x1 = t.getDepart().getX()*this.widthRatio;
+				double y1 = t.getDepart().getY()*this.heightRatio;
+				double x2 = t.getArrivee().getX()*this.widthRatio;
+				double y2 = t.getArrivee().getY()*this.heightRatio;
+				
+				g.setStroke(new BasicStroke((2f),
+								BasicStroke.CAP_BUTT,
+								BasicStroke.JOIN_ROUND));
+				g.setColor(new Color(red/255,green/255,blue/255));
+				green -= step;
+				Line2D.Double line = new Line2D.Double(x1, y1, x2, y2);
+				g.draw(line);
+				drawArrowHead(g, line);
+			}
+		}
+		
 	}
 	
 	public void dessinerEntrepot(Graphics2D g, Intersection i) {
@@ -107,6 +127,23 @@ public class VueGraphique extends Canvas implements Observer {
 		double x = i.getX()*this.widthRatio;
 		double y = i.getY()*this.heightRatio;
 		g.fill(new Ellipse2D.Double(x - VueGraphique.INTERSECTION_RADIUS/2, y - VueGraphique.INTERSECTION_RADIUS/2, VueGraphique.INTERSECTION_RADIUS, VueGraphique.INTERSECTION_RADIUS));
+	}
+	
+	public void drawArrowHead(Graphics2D g2d, Line2D.Double line) {
+		AffineTransform tx = new AffineTransform();
+		Polygon arrowHead = new Polygon();  
+		arrowHead.addPoint( 0,5);
+		arrowHead.addPoint( -5, -5);
+		arrowHead.addPoint( 5,-5);
+	    tx.setToIdentity();
+	    double angle = Math.atan2(line.y2-line.y1, line.x2-line.x1);
+	    tx.translate(line.x2, line.y2);
+	    tx.rotate((angle-Math.PI/2d));  
+	    
+	    Graphics2D g = (Graphics2D) g2d.create();
+	    g.setTransform(tx);   
+	    g.fill(arrowHead);
+	    g.dispose();
 	}
 	
 
